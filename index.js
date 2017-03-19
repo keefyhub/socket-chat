c738a29c6f7af832e31527d366a2eb830ac03f65
Keith Light <keith.light1986@gmail.com>
2017-03-19 22:15:17 +0000

    Add brunch and some default styles
    - Add brunch to watch and compile styles and scripts
    - Add some basic styles
    - Amend functions for both front and back end

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var app = require('express')();
+var express = require('express')
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
@@ -7,11 +8,15 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function(socket) {
   console.log('a user connected');
-  socket.on('disconnect', function(){
+
+  socket.on('disconnect', function() {
     console.log('user disconnected');
   });
+
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
     console.log('message: ' + msg);
